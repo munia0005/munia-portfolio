9f50f4404eecb7bf3c66879600ea0e90e08ac398
munia0005 <muniaaktar0005@gmail.com>
2024-10-12 23:54:18 +0600

    munia portfolio first commit

---
+++ data/index.ts
@@ -0,0 +1,238 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  // { name: "Testimonials", link: "#testimonials" },
+  { name: "Experience", link: "#experience" },
+  { name: "Approach", link: "#approach" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Flexible with different time zones around the world",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start mb-20",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Stack",
+    description: "My Tech",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2 pt-20 pb-20",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Patient Management System",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Cryptocurrency API Website",
+    des: "Real-Time Cryptocurrency Tracker with API Integration, using React.js & CSS",
+    img: "/crypto.png",
+    iconLists: ["/re.svg"],
+    link: "https://cryptoworld-pi.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Skin-Care E-Commerce Platform",
+    des: "Responsive Skin-Care E-Commerce Platform with Dynamic Cart and Checkout.",
+    img: "/05.png",
+    iconLists: ["/re.svg"],
+    link: "https://skin-care-shop.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Tutor Website",
+    des: "Dynamic Tutor Website with Teacher Filtering and Smooth Framer Motion Animations.",
+    img: "/04.png",
+    iconLists: ["/re.svg"],
+    link: "https://tutor-project-psi.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "GPT-3 Website",
+    des: "Fully Responsive Modern UI/UX Website Using React JS and CSS.",
+    img: "/03.png",
+    iconLists: ["/re.svg"],
+    link: "https://gpt-3-project-pi.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "Nike Website",
+    des: "Fully Responsive Modern Website in React JS With Tailwind.",
+    img: "/one.png",
+    iconLists: ["/re.svg", "/tail.svg"],
+    link: "https://nike-project-two.vercel.app/",
+  },
+  // {
+  //   id: 6,
+  //   title: "The Next Generation Payment Method Website",
+  //   des: "Responsive UI/UX Website in React JS with Tailwind.",
+  //   img: "/three.png",
+  //   iconLists: ["/re.svg", "/tail.svg"],
+  //   link: "https://bank-modern-app-sandy-nine.vercel.app/",
+  // },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  // {
+  //   id: 1,
+  //   title: "Frontend Engineer Intern",
+  //   desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp1.svg",
+  // },
+  // {
+  //   id: 2,
+  //   title: "Mobile App Dev - JSM Tech",
+  //   desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+  //   className: "md:col-span-2", // change to md:col-span-2
+  //   thumbnail: "/exp2.svg",
+  // },
+  {
+    id: 3,
+    title: "Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+  {
+    id: 4,
+    title: "SEO specialist",
+    desc: "Driving Organic Growth and Enhanced Search Engine Rankings.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/munia0005",
+  },
+  // {
+  //   id: 2,
+  //   img: "/twit.svg",
+  //   link : "https://best-gym-club.netlify.app/",
+  // },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/munia-aktar-468820283/",
+  },
+];
